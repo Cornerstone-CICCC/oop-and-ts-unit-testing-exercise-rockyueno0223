e6165adfeeac7871a854067653bbcdeff9806153
Hiroki Ueno <rockyueno@gmail.com>
2024-09-20 16:23:45 -0700

    updated rainCheck and convertTemp

--- tests/rainCheck.test.js
+++ tests/rainCheck.test.js
@@ -6,25 +6,31 @@ const rainCheck = require('../src/rainCheck')
 describe('Weather check', () => {
   test(`should return "Bring an umbrella" when it is raining`, () => {
     // expect here
+    expect(rainCheck(true))
   });
 
   test(`should return "No need for an umbrella" when it is not raining`, () => {
     // expect here
+    expect(rainCheck(false))
   });
 
   test(`should return "Bring a sturdy umbrella, it's windy." when it's raining and windy`, () => {
     // expect here
+    expect(rainCheck(true, 25))
   });
 
   test(`should return "Bring an umbrella and a warm coat, it's cold." when it's raining and cold`, () => {
     // expect here
+    expect(rainCheck(true, 0, 20))
   });
 
   test(`should return "No umbrella needed, but stay hydrated—it's hot outside!" when it's hot and not raining`, () => {
     // expect here
+    expect(rainCheck(false, 35))
   });
 
   test(`should return "No need for an umbrella, but it's quite windy. Hold onto your hat!" when it's windy and not raining`, () => {
     // expect here
+    expect(rainCheck(false, 0, 35))
   });
 });
