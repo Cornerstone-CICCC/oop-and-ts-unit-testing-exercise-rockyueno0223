4576f362a6d5fd191b05969c7faf94bc9be6547a
Hiroki Ueno <rockyueno@gmail.com>
2024-09-20 16:36:48 -0700

    updated counter

--- tests/counter.test.js
+++ tests/counter.test.js
@@ -5,3 +5,11 @@
 // HINT: Use the jest `resolves` matcher so that it is easier to work with async functions
 
 const counter = require('../src/counter')
+
+test("counter(10, 'increment') will eventualy return 10", () => {
+  return expect(counter(10, 'increment')).resolves.toBe(10)
+}, 11000)
+
+test("counter(10, 'decrement') will eventualy return 0", () => {
+  return expect(counter(10, 'decrement')).resolves.toBe(0)
+}, 11000)
